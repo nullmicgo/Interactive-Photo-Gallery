bbf3cdb8119b88b5f1e0f6686bd0fd8cb88ed3f1
nullmicgo@gmail.com <nullmicgo@gmail.com>
2017-10-04 00:49:24 +0800

    Updated the search function
    Updated the search function

--- js/main.js
+++ js/main.js
@@ -1,47 +1,51 @@
  lightbox.option({
-     'alwaysShowNavOnTouchDevices': true,
-     'positionFromTop': 100,
-     'maxWidth': 600,
-     'resizeDuration': 200,
-     'wrapAround': true,
-     'showImageNumberLabel': false
- });
+        'alwaysShowNavOnTouchDevices':true,
+        'positionFromTop':100,
+       'maxWidth':600,
+       'resizeDuration': 200,
+       'wrapAround': true,
+       'showImageNumberLabel':false
+  });
 
 
+function handleSearch(element){
+   var keywords =element.val() ;
+    $('.gallery-item').each(function () {
+
+      if(keywords==""){
+          $(this).show();
+      }else{
+         var title = $(this).find("img").attr("alt");
+         var description = $(this).attr('data-title');
+         var shouldShould =false;
+
+        if(title.indexOf(keywords) !== -1){
+              //found it
+             shouldShould= true;
+          }
+
+        if(description.indexOf(keywords) !== -1){
+              //found it
+             shouldShould= true;
+          }
+         if(shouldShould==true){
+            //found it
+            $(this).show();
+          }
+          else{
+            $(this).hide();
+          }
+      }
+        
+    });
+}
 
 
- $("input[type='text']").keypress(function() {
+$( "input[type='text']" ).keypress(function() {
+  handleSearch($(this));
+});
 
-     var keywords = $(this).val();
+$("input[type='text']").on('change', function(){
+  handleSearch($(this));
+});
 
-     $('.gallery-item').each(function() {
-
-         if (keywords == "") {
-             $(this).show();
-         } else {
-             var title = $(this).find("img").attr("alt");
-             var description = $(this).attr('data-title');
-             var shouldShould = false;
-
-             if (title.indexOf(keywords) !== -1) {
-                 //found it
-                 shouldShould = true;
-             }
-
-             if (description.indexOf(keywords) !== -1) {
-                 //found it
-                 shouldShould = true;
-             }
-             if (shouldShould == true) {
-                 //found it
-                 $(this).show();
-             } else {
-                 $(this).hide();
-             }
-         }
-
-     });
-
-
-
- });
